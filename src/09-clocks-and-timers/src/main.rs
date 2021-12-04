bd91337f6b1deaeed1ec0c7a222ffa2b855d0f4e
wjjmjh <u6269649@anu.edu.au>
2021-12-04 17:56:56 +1100

    BUILD: fancy flushing with delay.

--- src/09-clocks-and-timers/src/main.rs
+++ src/09-clocks-and-timers/src/main.rs
@@ -5,7 +5,16 @@ use aux9::{entry, switch_hal::OutputSwitch, tim6};
 
 #[inline(never)]
 fn delay(tim6: &tim6::RegisterBlock, ms: u16) {
-    // TODO implement this
+    // Set the timer to go off in `ms` ticks
+    tim6.arr.write(|w| w.arr().bits(ms));
+
+    // CEN: Enable the counter
+    tim6.cr1.modify(|_, w| w.cen().set_bit());
+
+    // Wait until the alarm goes off (until the update event occurs)
+    while !tim6.sr.read().uif().bit_is_set() {}
+
+    tim6.sr.modify(|_, w| w.uif().clear_bit());
 }
 
 #[entry]
@@ -13,13 +22,18 @@ fn main() -> ! {
     let (leds, rcc, tim6) = aux9::init();
     let mut leds = leds.into_array();
 
-    // TODO initialize TIM6
+    rcc.apb1enr.modify(|_, w| w.tim6en().set_bit());
+
+    // OPM Select one pulse mode
+    // CEN Keep the counter disabled for now
+    tim6.cr1.write(|w| w.opm().set_bit().cen().clear_bit());
+
+    tim6.psc.write(|w| w.psc().bits(7_999));
 
     let ms = 50;
     loop {
         for curr in 0..8 {
             let next = (curr + 1) % 8;
-
             leds[next].on().unwrap();
             delay(tim6, ms);
             leds[curr].off().unwrap();
